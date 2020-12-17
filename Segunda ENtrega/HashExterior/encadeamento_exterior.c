128c9622cac9e14119addb00702ad5ea4f50446f
Victor01Lima <victorluz.lima@gmail.com>
2020-12-17 20:35:20 -0300

    funcao cria_hash feita

--- Segunda ENtrega/HashExterior/encadeamento_exterior.c
+++ Segunda ENtrega/HashExterior/encadeamento_exterior.c
@@ -1,15 +1,27 @@
 #ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS
 #endif
-
+#include<stdio.h>
 #include <limits.h>
-
+#include "cliente.h"
 #include "encadeamento_exterior.h"
 
 
 void cria_hash(char *nome_arquivo_hash, int tam)
 {
 	//TODO: criar a tabela hash
+	// cria um vetor dinamicamente do tipo Cliente
+	Cliente *Cliente_hash[tam];
+	//arquivo de saida é aberto
+	FILE *out = fopen("TabelaHash.dat", "wb");
+	// laço de repetição para salvar cada posicao do nosso vetor dinamico no arquivo de saida
+	for(int i=0 ;i<tam;i++)
+	{
+		// cada posicao do vetor é definida como NULL
+		Cliente_hash[i]=NULL;
+		salva_cliente(Cliente_hash[i],out);
+	}
+	fclose(out);
 }
 
 int busca(int cod_cli, char *nome_arquivo_hash, char *nome_arquivo_dados)
