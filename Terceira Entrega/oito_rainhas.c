da49e495e963b782b4f25a8a2c86570638fc06b4
Victor01Lima <victorluz.lima@gmail.com>
2021-02-03 09:22:56 -0300

    arquivos base

---
+++ Terceira Entrega/oito_rainhas.c
@@ -0,0 +1,15 @@
+
+// Atividade em grupo - Estrutura de Dados II
+// Feito por Victor da Luz Lima, ...
+
+
+
+#include<stdbool.h>
+#include<stdio.h>
+
+int main(){
+
+
+
+    return 0;
+}
---
+++ Terceira Entrega/permutacoes.c
@@ -0,0 +1,15 @@
+
+// Atividade em grupo - Estrutura de Dados II
+// Feito por , ...
+
+
+
+#include<stdbool.h>
+#include<stdio.h>
+
+int main(){
+
+
+
+    return 0;
+}
